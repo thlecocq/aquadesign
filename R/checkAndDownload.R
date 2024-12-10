0aee9df55051fc7b0f024987c894144da3ac8460
thlecocq <thomas.lecocq@univ-lorraine.fr>
2024-12-10 13:45:25 +0100

    Add files via upload
    Function to download abiotics databases if they haven't been already.

---
+++ R/checkAndDownload.R
@@ -0,0 +1,22 @@
+#' Title Download datasets from various databases
+#'
+#' @param url Url to download
+#' @param filePath path to put the downloaded file
+#'
+#'@importFrom utils download.file
+#' @return
+#' @export
+#'
+#' @examples
+
+checkAndDownload <- function(url,filePath) {
+    if (!file.exists(filePath)) {
+      tryCatch(download.file(url,filePath, mode = "wb", quiet = FALSE), 
+        error = function(e) print(paste0("The download failed on url ",url)))
+        quit()
+    }
+    else{
+      warningMsg=paste0("The file ",filePath," already exist. If you wish to update it, please remove this file and rerun Aquadesign.")
+      cat(warningMsg)
+    }
+}
